15c672802350a1207d181d270ea4a2ce365a60a6
Matthias Kretz <kretz@kde.org>
2008-05-16 13:44:58 +0000

    - copy non-KDE parts of kdelibs/phonon to kdesupport which will become the   never-freezing new home of Phonon - adjust buildsystem to work without cmake macros in kdelibs - install platformplugin.h which is officially public API (actually already public with Qt 4.4)

---
+++ phonon/tests/methods/mediacontroller.cpp
@@ -0,0 +1,8 @@
+addSignal("availableTitlesChanged(int)");
+addSignal("availableAnglesChanged(int)");
+addSignal("angleChanged(int)");
+addSignal("availableAudioChannelsChanged()");
+addSignal("chapterChanged(int)");
+addSignal("titleChanged(int)");
+addSignal("availableChaptersChanged(int)");
+addSignal("availableSubtitlesChanged()");
