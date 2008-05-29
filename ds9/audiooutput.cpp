3be5f330c5392a1876a76422112a5eb689c3a2d6
Christian Ehrlicher <Ch.Ehrlicher@gmx.de>
2008-05-29 17:06:36 +0000

    some fixes, still doesn't compile on windows

--- ds9/audiooutput.cpp
+++ ds9/audiooutput.cpp
@@ -15,6 +15,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with this library.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "audiooutput_p.h"
 #include "audiooutput.h"
 #include "mediaobject.h"
 
