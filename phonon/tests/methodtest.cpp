e1949ff144bf3b94fc9adde0e84da41adb27bc05
Jonathan Riddell <jr@jriddell.org>
2008-06-12 14:50:40 +0000

    Standardise on LGPL 2.1, 3 or later as approved by licence Add copy of licence Add cmake modules licence cmake/COPYING-CMAKE-SCRIPTS

--- phonon/tests/methodtest.cpp
+++ phonon/tests/methodtest.cpp
@@ -3,8 +3,12 @@
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation; either version 2 of 
-    the License, or (at your option) version 3.
+    published by the Free Software Foundation; either version 2 of
+    the License or (at your option) version 3 or any later version
+    accepted by the membership of KDE e.V. (or its successor approved
+    by the membership of KDE e.V.), Trolltech ASA (or its successors, 
+    if any) and the KDE Free Qt Foundation, which shall act as a proxy 
+    defined in Section 14 of version 3 of the license.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
