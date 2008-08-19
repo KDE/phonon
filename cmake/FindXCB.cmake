7fd6c21f758c19056bf6b597ce32435df9158c49
Helio Chissini de Castro <helio@kde.org>
2008-08-19 22:19:25 +0000

    xine almost back. Just disabling for now

--- cmake/FindXCB.cmake
+++ cmake/FindXCB.cmake
@@ -6,7 +6,6 @@
 #  LIBXCB_INCLUDE_DIR - the libxcb include dir
 #  LIBXCB_DEFINITIONS - compiler switches required for using libxcb
 
-# Copyright (c) 2008 Helio Chissini de Castro, <helio@kde.org>
 # Copyright (c) 2007, Matthias Kretz, <kretz@kde.org>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
@@ -21,18 +20,18 @@ IF (NOT WIN32)
 
   # use pkg-config to get the directories and then use these values
   # in the FIND_PATH() and FIND_LIBRARY() calls
-  FIND_PACKAGE(PkgConfig)
-  PKG_CHECK_MODULES(PKG_XCB xcb)
-
-  SET(LIBXCB_DEFINITIONS ${PKG_XCB_CFLAGS})
+  INCLUDE(UsePkgConfig)
+  PKGCONFIG(xcb _LibXCBIncDir _LibXCBLinkDir _LibXCBLinkFlags _LibXCBCflags)
+	  
+  SET(LIBXCB_DEFINITIONS ${_LibXCBCflags})
 
   FIND_PATH(LIBXCB_INCLUDE_DIR xcb/xcb.h
-    ${PKG_XCB_INCLUDE_DIRS}
+    ${_LibXCBIncDir}
     )
 
   FIND_LIBRARY(LIBXCB_LIBRARIES NAMES xcb libxcb
     PATHS
-    ${PKG_XCB_LIBRARY_DIRS}
+    ${_LibXCBLinkDir}
     )
 
   include(FindPackageHandleStandardArgs)
