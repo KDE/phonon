fe266c106f71240a6dd5b88facf8e24ea93d08f0
Matthias Kretz <kretz@kde.org>
2008-11-05 09:10:12 +0000

    docu for signals and the new helper function

--- phonon/audiooutputinterface.h
+++ phonon/audiooutputinterface.h
@@ -36,6 +36,14 @@ namespace Phonon
 /** \class AudioOutputInterface audiooutputinterface.h Phonon/AudioOutputInterface
  * \short Interface for AudioOutput objects
  *
+ * The implementation can make use of the signals
+ * \code
+     void volumeChanged(qreal newVolume);
+     void audioDeviceFailed();
+ * \endcode
+ * to notify the frontend whenever the volume has changed or when an audioDeviceFailed (e.g. USB
+ * unplug or sound server failure).
+ *
  * \author Matthias Kretz <kretz@kde.org>
  */
 class AudioOutputInterface40
@@ -55,6 +63,9 @@ class AudioOutputInterface40
          *
          * A value of 0.0 means muted, 1.0 means unchanged, 2.0 means double voltage (i.e. all
          * samples are multiplied by 2).
+         *
+         * Everytime the volume in the backend changes it should emit volumeChanged(qreal), also
+         * inside this function.
          */
         virtual void setVolume(qreal) = 0;
 
@@ -90,6 +101,35 @@ class AudioOutputInterface42 : public AudioOutputInterface40
 
         using AudioOutputInterface40::setOutputDevice;
 
+        /**
+         * Helper function for backends to get a list of (driver, handle) pairs for
+         * AudioOutputDevice objects that are listed by the platform plugin.
+         *
+         * Example:
+         * \code
+           typedef QPair<QByteArray, QString> PhononDeviceAccess;
+           const QList<PhononDeviceAccess> &deviceAccessList = deviceAccessListFor(deviceDesc);
+           foreach (const PhononDeviceAccess &access, deviceAccessList) {
+               const QByteArray &driver = access.first;
+               const QString &handle = access.second;
+               if (openDevice(driver, handle)) {
+                   // we found the first pair in the list that works. done.
+                   return;
+               }
+               // continue trying the other (driver, handle) pairs
+           }
+           // none of the (driver, handle) pairs worked, that means the whole AudioOutputDevice is
+           // inaccessible and the frontend needs to know (either by emitting audioDeviceFailed or
+           // returning false when called from setOutputDevice)
+         * \endcode
+         *
+         * At the time of this writing the following driver strings are known to be in use:
+         * \li \c alsa: The handle is the string to pass to snd_pcm_open (e.g. "dmix:CARD=0,DEV=1")
+         * \li \c oss: The handle is the device file (e.g. "/dev/dsp")
+         * \li \c pulseaudio: The handle contains the server string and the sink/source name
+         * separated by a newline character.
+         * (e.g. unix:/tmp/pulse-mkretz/native\nalsa_output.pci_8086_293e_sound_card_0_alsa_playback_0)
+         */
         PHONON_EXPORT QList<QPair<QByteArray, QString> > deviceAccessListFor(const Phonon::AudioOutputDevice &) const;
 };
 
