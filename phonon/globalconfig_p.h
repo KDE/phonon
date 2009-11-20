5d6f9f6616755c7d43cd5a1a9c60ef629452b2f9
Colin Guthrie <kde@colin.guthr.ie>
2009-11-20 00:08:36 +0000

    config: provide an API to allow clients to adjust the HideAdvancedDevices preference

--- phonon/globalconfig_p.h
+++ phonon/globalconfig_p.h
@@ -46,13 +46,15 @@ namespace Phonon
             AdvancedDevicesFromSettings = 2,
             HideUnavailableDevices = 4
         };
-        bool isHiddenAudioOutputDevice(int i);
+        bool getHideAdvancedDevices() const;
+        void hideAdvancedDevices(bool hide = true);
+        bool isHiddenAudioOutputDevice(int i) const;
         void setAudioOutputDeviceListFor(Phonon::Category category, QList<int> order);
         QList<int> audioOutputDeviceListFor(Phonon::Category category, int override = AdvancedDevicesFromSettings) const;
         int audioOutputDeviceFor(Phonon::Category category, int override = AdvancedDevicesFromSettings) const;
 
 #ifndef QT_NO_PHONON_AUDIOCAPTURE
-        bool isHiddenAudioCaptureDevice(int i);
+        bool isHiddenAudioCaptureDevice(int i) const;
         void setAudioCaptureDeviceListFor(Phonon::Category category, QList<int> order);
         QList<int> audioCaptureDeviceListFor(Phonon::Category category, int override = AdvancedDevicesFromSettings) const;
         int audioCaptureDeviceFor(Phonon::Category category, int override = AdvancedDevicesFromSettings) const;
