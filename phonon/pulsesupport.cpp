9edcb7272d29e6efb0f023410533e0efdebf68f3
Colin Guthrie <kde@colin.guthr.ie>
2009-11-20 00:08:50 +0000

    pulseaudio: Process the device list from pulseaudio and maintain an internal cache.
    This commit ensures that the information obtained from pulseaudio is kept up-to-date and ordered by priority
    as defined by the pulseaudio server. This information will ultimately be exposed via the globalconfig API
    so that the GUI on KDE will show a proper list of devices.

--- phonon/pulsesupport.cpp
+++ phonon/pulsesupport.cpp
@@ -20,19 +20,19 @@
 
 */
 
-#include <stdio.h>
-#include <glib.h>
 #include <QtCore/QtGlobal>
 #include <QtCore/QEventLoop>
 #include <QtCore/QDebug>
 
 #ifdef HAVE_PULSEAUDIO
+#include <glib.h>
 #include <pulse/pulseaudio.h>
 #include <pulse/glib-mainloop.h>
 #include <pulse/ext-device-manager.h>
 #endif // HAVE_PULSEAUDIO
 
 #include "pulsesupport_p.h"
+#include "phononnamespace.h"
 
 QT_BEGIN_NAMESPACE
 
@@ -40,25 +40,6 @@ namespace Phonon
 {
 
 #ifdef HAVE_PULSEAUDIO
-class PulseUserData
-{
-    public:
-        inline 
-        PulseUserData(QEventLoop *eventLoop)
-            : m_eventLoop(eventLoop)
-        {
-            Q_ASSERT(eventLoop);
-        }
-
-        inline void initialConnectionLoopExit()
-        {
-            m_eventLoop->exit(0);
-            m_eventLoop = NULL;
-        }
-    private:
-        QEventLoop *m_eventLoop;
-};
-
 /***
 * Prints a conditional debug message based on the current debug level
 * If obj is provided, classname and objectname will be printed as well
@@ -93,41 +74,182 @@ static void logMessage(const QString &message, int priority, QObject *obj)
     }
 }
 
+
+class AudioDevice
+{
+    public:
+        inline
+        AudioDevice(QString name, unsigned int priority, QString desc, QString icon)
+        : pulseName(name)
+        , pulsePriority(priority)
+        , description(desc)
+        , icon(icon)
+        , isAdvanced(false)
+        {
+            /*logMessage(QString("Adding '%1' (priority: %2)").arg(desc).arg(priority));*/
+        }
+
+        QString pulseName;
+        unsigned int pulsePriority;
+        QString description;
+        QString icon;
+        bool isAdvanced;
+};
+bool operator<(const AudioDevice &a, const AudioDevice &b)
+{
+    return a.pulsePriority < b.pulsePriority;
+}
+
+class PulseUserData
+{
+    public:
+        inline 
+        PulseUserData()
+        {
+        }
+
+        QMap<Phonon::Category, QList<AudioDevice> > outputDevices;
+        QMap<Phonon::Category, QList<AudioDevice> > captureDevices;
+};
+
+static QMap<QString, Phonon::Category> s_role_category_map;
+
 static PulseSupport* s_instance = NULL;
 static bool s_pulseActive = false;
 
 static pa_glib_mainloop *s_mainloop = NULL;
 static pa_context *s_context = NULL;
+static QEventLoop *s_connection_eventloop = NULL;
 
-static void ext_device_manager_read_cb(pa_context *c, const pa_ext_device_manager_info *info, int eol, void *) {
+QMap<Phonon::Category, QList<AudioDevice> > s_output_devices;
+QMap<Phonon::Category, QList<AudioDevice> > s_capture_devices;
+
+
+static void ext_device_manager_subscribe_cb(pa_context *, void *);
+static void ext_device_manager_read_cb(pa_context *c, const pa_ext_device_manager_info *info, int eol, void *userdata) {
     Q_ASSERT(c);
+    Q_ASSERT(userdata);
+
+    // If this is our first iteration, set things up properly
+    if (s_connection_eventloop) {
+        s_connection_eventloop->exit(0);
+        s_connection_eventloop = NULL;
+        s_pulseActive = true;
+
+        pa_operation *o;
+        pa_ext_device_manager_set_subscribe_cb(c, ext_device_manager_subscribe_cb, NULL);
+        if ((o = pa_ext_device_manager_subscribe(c, 1, NULL, NULL)))
+            pa_operation_unref(o);
+    }
 
     if (eol < 0) {
         logMessage(QString("Failed to initialize device manager extension: %1").arg(pa_strerror(pa_context_errno(c))));
+        s_pulseActive = false;
         return;
+    }
+
+    PulseUserData *u = reinterpret_cast<PulseUserData*>(userdata);
+    if (eol) {
+        // We're done reading the data, so order it by priority and copy it into the
+        // static variables where it can then be accessed by those classes that need it.
+
+        QMap<Phonon::Category, QList<AudioDevice> >::iterator it;
+        for (it = u->outputDevices.begin(); it != u->outputDevices.end(); ++it) {
+            QList<AudioDevice> &list = it.value();
+            qSort(list);
+        }
+        s_output_devices = u->outputDevices;
+        for (it = u->captureDevices.begin(); it != u->captureDevices.end(); ++it) {
+            QList<AudioDevice> &list = it.value();
+            qSort(list);
+        }
+        s_capture_devices = u->captureDevices;
+
+        // Also free the user data as we will not be called again.
+        delete u;
+
+        // Some debug
+        logMessage(QString("Output Device Priority List:"));
+        for (int i = Phonon::NoCategory; i <= Phonon::LastCategory; ++i) {
+            Phonon::Category cat = static_cast<Phonon::Category>(i);
+            if (s_output_devices.contains(cat)) {
+                logMessage(QString("  Phonon Category %1").arg(cat));
+                QList<AudioDevice>::iterator it;
+                int j = 0;
+                for (it = s_output_devices[cat].begin(); it != s_output_devices[cat].end(); ++it) {
+                    logMessage(QString("    %1. %2").arg(++j).arg((*it).description));
+                }
+            }
+        }
+        logMessage(QString("Capture Device Priority List:"));
+        for (int i = Phonon::NoCategory; i <= Phonon::LastCategory; ++i) {
+            Phonon::Category cat = static_cast<Phonon::Category>(i);
+            if (s_capture_devices.contains(cat)) {
+                logMessage(QString("  Phonon Category %1").arg(cat));
+                QList<AudioDevice>::iterator it;
+                int j = 0;
+                for (it = s_capture_devices[cat].begin(); it != s_capture_devices[cat].end(); ++it) {
+                    logMessage(QString("    %1. %2").arg(++j).arg((*it).description));
+                }
+            }
+        }
     }
 
     if (!info)
         return;
 
-    /* Do something with a widget when this part is written */
-    logMessage(QString("Found: %1 '%2'").arg(info->name).arg(info->description));
+    Q_ASSERT(info->name);
+    Q_ASSERT(info->description);
+    Q_ASSERT(info->icon);
+
+    // For each role in the priority, map it to a phonon category and store the order.
+    for (uint32_t i = 0; i < info->n_role_priorities; ++i) {
+        pa_ext_device_manager_role_priority_info* role_prio = &info->role_priorities[i];
+        Q_ASSERT(role_prio->role);
+
+        if (s_role_category_map.contains(role_prio->role)) {
+            Phonon::Category cat = s_role_category_map[role_prio->role];
+            QList<AudioDevice> *list;
+            QString name(info->name);
+
+            if (name.startsWith("sink:")) {
+                if (!u->outputDevices.contains(cat))
+                    u->outputDevices.insert(cat, QList<AudioDevice>());
+                list = &u->outputDevices[cat];
+            } else if (name.startsWith("source:")) {
+                if (!u->captureDevices.contains(cat))
+                    u->captureDevices.insert(cat, QList<AudioDevice>());
+                list = &u->captureDevices[cat];
+            } else {
+                continue;
+            }
+
+            list->append(AudioDevice(info->name, role_prio->priority, info->description, info->icon));
+        }
+    }
 }
 
-static void ext_device_manager_subscribe_cb(pa_context *c, void *userdata) {
+static void ext_device_manager_subscribe_cb(pa_context *c, void *) {
     Q_ASSERT(c);
 
     pa_operation *o;
-    if (!(o = pa_ext_device_manager_read(c, ext_device_manager_read_cb, userdata))) {
+    PulseUserData *u = new PulseUserData; /** @todo Make some object to receive the info... */
+    if (!(o = pa_ext_device_manager_read(c, ext_device_manager_read_cb, u))) {
+        // We need to deal with failure on first iteration
+        if (s_connection_eventloop) {
+            s_connection_eventloop->exit(0);
+            s_connection_eventloop = NULL;
+        }
         logMessage(QString("pa_ext_device_manager_read() failed"));
         return;
     }
     pa_operation_unref(o);
 }
 
-static void context_state_callback(pa_context *c, void *userdata)
+static void context_state_callback(pa_context *c, void *)
 {
-    PulseUserData *u = reinterpret_cast<PulseUserData *>(userdata);
+    Q_ASSERT(c);
+
     switch (pa_context_get_state(c)) {
         case PA_CONTEXT_UNCONNECTED:
         case PA_CONTEXT_CONNECTING:
@@ -136,27 +258,16 @@ static void context_state_callback(pa_context *c, void *userdata)
             break;
 
         case PA_CONTEXT_READY:
-            u->initialConnectionLoopExit();
-
-            pa_operation *o;
-
-            if ((o = pa_ext_device_manager_read(c, ext_device_manager_read_cb, u))) {
-                pa_operation_unref(o);
-
-                pa_ext_device_manager_set_subscribe_cb(c, ext_device_manager_subscribe_cb, u);
-
-                if ((o = pa_ext_device_manager_subscribe(c, 1, NULL, NULL)))
-                    pa_operation_unref(o);
-
-            } else
-                logMessage(QString("Failed to initialize stream_restore extension: %1").arg(pa_strerror(pa_context_errno(c))));
-
-            s_pulseActive = true;
+            // Attempt to load things up
+            ext_device_manager_subscribe_cb(c, NULL);
             break;
 
         case PA_CONTEXT_FAILED:
             s_pulseActive = false;
-            u->initialConnectionLoopExit();
+            if (s_connection_eventloop) {
+                s_connection_eventloop->exit(0);
+                s_connection_eventloop = NULL;
+            }
             break;
 
         case PA_CONTEXT_TERMINATED:
@@ -166,10 +277,8 @@ static void context_state_callback(pa_context *c, void *userdata)
             break;
     }
 }
-
-
-
 #endif // HAVE_PULSEAUDIO
+
 
 PulseSupport* PulseSupport::getInstance()
 {
@@ -189,6 +298,16 @@ void PulseSupport::shutdown()
 
 PulseSupport::PulseSupport()
 {
+    s_role_category_map["none"] = Phonon::NoCategory;
+    s_role_category_map["video"] = Phonon::VideoCategory;
+    s_role_category_map["music"] = Phonon::MusicCategory;
+    s_role_category_map["game"] = Phonon::GameCategory;
+    s_role_category_map["event"] = Phonon::NotificationCategory;
+    s_role_category_map["phone"] = Phonon::CommunicationCategory;
+    //s_role_category_map["animation"] = Phonon::NoCategory; // No Mapping
+    //s_role_category_map["production"] = Phonon::NoCategory; // No Mapping
+    s_role_category_map["a11y"] = Phonon::AccessibilityCategory;
+
 #ifdef HAVE_PULSEAUDIO
     // To allow for easy debugging, give an easy way to disable this pulseaudio check
     QString pulseenv = qgetenv("PHONON_DISABLE_PULSEAUDIO");
@@ -201,17 +320,16 @@ PulseSupport::PulseSupport()
 
     // We create a simple event loop to allow the glib loop
     // to iterate until we've connected or not to the server.
-    QEventLoop loop;
-    PulseUserData* userData = new PulseUserData(&loop);
+    s_connection_eventloop = new QEventLoop;
 
     // XXX I don't want to show up in the client list. All I want to know is the list of sources
     // and sinks...
     s_context = pa_context_new(api, "libphonon");
     // (cg) Convert to PA_CONTEXT_NOFLAGS when PulseAudio 0.9.19 is required
     if (pa_context_connect(s_context, NULL, static_cast<pa_context_flags_t>(0), 0) >= 0) {
-        pa_context_set_state_callback(s_context, &context_state_callback, userData);
+        pa_context_set_state_callback(s_context, &context_state_callback, s_connection_eventloop);
         // Now we block until we connect or otherwise...
-        loop.exec();
+        s_connection_eventloop->exec();
     }
 #endif
 }
@@ -227,6 +345,11 @@ PulseSupport::~PulseSupport()
     if (s_mainloop) {
         pa_glib_mainloop_free(s_mainloop);
         s_mainloop = NULL;
+    }
+
+    if (s_connection_eventloop) {
+        delete s_connection_eventloop;
+        s_connection_eventloop = NULL;
     }
 #endif
 }
