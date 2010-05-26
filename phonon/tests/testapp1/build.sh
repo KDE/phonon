5fc6133e3ab0f311a970a0deea02613a0b625047
Casian Andrei <skeletk13@gmail.com>
2010-05-26 13:19:52 +0300

    Removed stupid clear command from the mini build script

--- phonon/tests/testapp1/build.sh
+++ phonon/tests/testapp1/build.sh
@@ -1,1 +1,1 @@
-clear; moc phonon_test.h > moc_phonon_test.cxx; g++ -o phonon_test -Wall -g -lkdecore -lQtCore -lQtGui -lphonon moc_phonon_test.cxx phonon_test.cpp main.cpp 
+moc phonon_test.h > moc_phonon_test.cxx; g++ -o phonon_test -Wall -g -lkdecore -lQtCore -lQtGui -lphonon moc_phonon_test.cxx phonon_test.cpp main.cpp 
