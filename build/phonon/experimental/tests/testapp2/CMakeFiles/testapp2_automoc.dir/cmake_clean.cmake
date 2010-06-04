3195075828784765ee84c57cfd0a2c14d6f6339f
Casian Andrei <skeletk13@gmail.com>
2010-06-04 23:45:06 +0300

    Revert "Added test using phonon experimental". Build folder added by mistake
    This reverts commit c9c72c368e02cf9a34bc85113b7d062967bdf920.

--- build/phonon/experimental/tests/testapp2/CMakeFiles/testapp2_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "testapp2_automoc.cpp"
-  "CMakeFiles/testapp2_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/testapp2_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
