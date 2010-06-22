24c1f8213f8d369e4b2a03425725a6e18375a713
Casian Andrei <skeletk13@gmail.com>
2010-06-22 21:56:43 +0300

    Removed useless application test

--- phonon/tests/testapp1/build.sh
+++
@@ -1,2 +0,0 @@
-moc phonon_test.h > moc_phonon_test.cxx
-g++ -o phonon_test -Wall -g -lkdecore -lQtCore -lQtGui -lphononexperimental moc_phonon_test.cxx phonon_test.cpp main.cpp 
--- phonon/tests/testapp1/clean.sh
+++
@@ -1,4 +0,0 @@
-rm -f *~
-rm -f phonon_test
-rm -f *.o
-rm -f *.cxx
