bbcc201aeaf3b02ed98688a573c478bea3589cb8
Martin T. H. Sandsmark <sandsmark@samfundet.no>
2010-08-12 19:22:46 +0200

    Adopt the FindGLIB2.cmake from kdelibs.
    BUG: 247546

--- cmake/FindGLIB2.cmake
+++ cmake/FindGLIB2.cmake
@@ -5,36 +5,45 @@
 #  GLIB2_INCLUDE_DIR - the glib2 include directory
 #  GLIB2_LIBRARIES - glib2 library
 
+# Copyright (c) 2008 Laurent Montel, <montel@kde.org>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
 
 if(GLIB2_INCLUDE_DIR AND GLIB2_LIBRARIES)
     # Already in cache, be silent
     set(GLIB2_FIND_QUIETLY TRUE)
 endif(GLIB2_INCLUDE_DIR AND GLIB2_LIBRARIES)
 
-if (NOT WIN32)
-   find_package(PkgConfig REQUIRED)
-   pkg_check_modules(PKG_GLIB REQUIRED glib-2.0)
-endif(NOT WIN32)
+find_package(PkgConfig)
+pkg_check_modules(PC_LibGLIB2 QUIET glib-2.0)
 
-find_path(GLIB2_MAIN_INCLUDE_DIR glib.h
-          PATH_SUFFIXES glib-2.0
-          PATHS ${PKG_GLIB_INCLUDE_DIRS} )
+find_path(GLIB2_MAIN_INCLUDE_DIR
+          NAMES glib.h
+          HINTS ${PC_LibGLIB2_INCLUDEDIR}
+          PATH_SUFFIXES glib-2.0)
+
+find_library(GLIB2_LIBRARY 
+             NAMES glib-2.0 
+             HINTS ${PC_LibGLIB2_LIBDIR}
+)
+
+set(GLIB2_LIBRARIES ${GLIB2_LIBRARY})
 
 # search the glibconfig.h include dir under the same root where the library is found
-find_library(GLIB2_LIBRARIES
-             NAMES glib-2.0
-             PATHS ${PKG_GLIB_LIBRARY_DIRS} )
+get_filename_component(glib2LibDir "${GLIB2_LIBRARIES}" PATH)
 
 find_path(GLIB2_INTERNAL_INCLUDE_DIR glibconfig.h
-          PATH_SUFFIXES glib-2.0/include ../lib/glib-2.0/include
-          PATHS ${PKG_GLIB_INCLUDE_DIRS} ${PKG_GLIB_LIBRARIES} ${CMAKE_SYSTEM_LIBRARY_PATH})
+          PATH_SUFFIXES glib-2.0/include
+          HINTS ${PC_LibGLIB2_INCLUDEDIR} "${glib2LibDir}" ${CMAKE_SYSTEM_LIBRARY_PATH})
 
-set(GLIB2_INCLUDE_DIR ${GLIB2_MAIN_INCLUDE_DIR})
+set(GLIB2_INCLUDE_DIR "${GLIB2_MAIN_INCLUDE_DIR}")
 
 # not sure if this include dir is optional or required
 # for now it is optional
 if(GLIB2_INTERNAL_INCLUDE_DIR)
-  set(GLIB2_INCLUDE_DIR ${GLIB2_INCLUDE_DIR} ${GLIB2_INTERNAL_INCLUDE_DIR})
+  set(GLIB2_INCLUDE_DIR ${GLIB2_INCLUDE_DIR} "${GLIB2_INTERNAL_INCLUDE_DIR}")
 endif(GLIB2_INTERNAL_INCLUDE_DIR)
 
 include(FindPackageHandleStandardArgs)
