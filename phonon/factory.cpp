5062b80e76d282cc65172eac6cc2152fad794508
Harald Sitter <sitter.harald@gmail.com>
2010-11-21 20:54:59 +0100

    warnings-- && pointlessincludes-- && trailingwhitespaces--

--- phonon/factory.cpp
+++ phonon/factory.cpp
@@ -6,7 +6,7 @@
     License as published by the Free Software Foundation; either
     version 2.1 of the License, or (at your option) version 3, or any
     later version accepted by the membership of KDE e.V. (or its
-    successor approved by the membership of KDE e.V.), Nokia Corporation 
+    successor approved by the membership of KDE e.V.), Nokia Corporation
     (or its successors, if any) and the KDE Free Qt Foundation, which shall
     act as a proxy defined in Section 6 of version 3 of the license.
 
@@ -15,7 +15,7 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public 
+    You should have received a copy of the GNU Lesser General Public
     License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 */
@@ -375,8 +375,9 @@ PlatformPlugin *FactoryPrivate::platformPlugin()
         QPluginLoader pluginLoader(QString::fromLocal8Bit(platform_plugin_env.constData()));
         if (pluginLoader.load()) {
             QObject *plInstance = pluginLoader.instance();
-            if (!plInstance)
+            if (!plInstance) {
                 pDebug() << Q_FUNC_INFO << "unable to grab root component object for the platform plugin";
+            }
 
             m_platformPlugin = qobject_cast<PlatformPlugin *>(plInstance);
             if (m_platformPlugin) {
