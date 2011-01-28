3df3f0c3fd7c3665752dce409b038bdda0299e1a
Harald Sitter <sitter@kde.org>
2011-01-28 15:37:33 +0100

    docmentation++ && formatting ++ && add meaningful menu enums

--- phonon/addoninterface.h
+++ phonon/addoninterface.h
@@ -1,12 +1,13 @@
 /*  This file is part of the KDE project
     Copyright (C) 2007-2008 Matthias Kretz <kretz@kde.org>
+    Copyright (C) 2011 Harald Sitter <sitter@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
     License as published by the Free Software Foundation; either
     version 2.1 of the License, or (at your option) version 3, or any
     later version accepted by the membership of KDE e.V. (or its
-    successor approved by the membership of KDE e.V.), Nokia Corporation 
+    successor approved by the membership of KDE e.V.), Nokia Corporation
     (or its successors, if any) and the KDE Free Qt Foundation, which shall
     act as a proxy defined in Section 6 of version 3 of the license.
 
@@ -15,7 +16,7 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public 
+    You should have received a copy of the GNU Lesser General Public
     License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 */
@@ -46,27 +47,36 @@ class AddonInterface
         virtual ~AddonInterface() {}
 
         enum Interface {
-            NavigationInterface   = 1,
-            ChapterInterface      = 2,
-            AngleInterface        = 3,
-            TitleInterface        = 4,
-            SubtitleInterface     = 5,
-            AudioChannelInterface = 6
+            NavigationInterface   = 1, /**< Interface for (menu) navigation */
+            ChapterInterface      = 2, /**< Interface for chapter control */
+            AngleInterface        = 3, /**< Interface for angle control */
+            TitleInterface        = 4, /**< Interface for title control */
+            SubtitleInterface     = 5, /**< Interface for subtitle control */
+            AudioChannelInterface = 6  /**< Interface for audio channel control */
         };
 
         enum NavigationCommand {
-            Menu1Button
+            availableMenus,
+            MenuMain,
+            MenuTitle,
+            MenuRoot,
+            MenuAudio,
+            MenuAngle,
+            MenuChapter
         };
+
         enum ChapterCommand {
             availableChapters,
             chapter,
             setChapter
         };
+
         enum AngleCommand {
             availableAngles,
             angle,
             setAngle
         };
+
         enum TitleCommand {
             availableTitles,
             title,
@@ -74,11 +84,13 @@ class AddonInterface
             autoplayTitles,
             setAutoplayTitles
         };
+
         enum SubtitleCommand {
             availableSubtitles,
             currentSubtitle,
             setCurrentSubtitle
         };
+
         enum AudioChannelCommand {
             availableAudioChannels,
             currentAudioChannel,
