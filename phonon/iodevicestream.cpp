3158f6e702092a69a5b81a32d1960b42f120eaf4
Harald Sitter <sitter@kde.org>
2011-02-22 13:39:44 +0100

    do not got to error state if nothing was read but we are not at EOS, we get 0 byte reads if the device is not yet ready to read... possibly we should wait for readyread really?

--- phonon/iodevicestream.cpp
+++ phonon/iodevicestream.cpp
@@ -6,7 +6,7 @@
     License as published by the Free Software Foundation; either
     version 2.1 of the License, or (at your option) version 3, or any
     later version accepted by the membership of KDE e.V. (or its
-    successor approved by the membership of KDE e.V.), Nokia Corporation 
+    successor approved by the membership of KDE e.V.), Nokia Corporation
     (or its successors, if any) and the KDE Free Qt Foundation, which shall
     act as a proxy defined in Section 6 of version 3 of the license.
 
@@ -15,7 +15,7 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public 
+    You should have received a copy of the GNU Lesser General Public
     License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 */
@@ -73,9 +73,12 @@ void IODeviceStream::needData()
     quint32 size = 4096;
     Q_D(IODeviceStream);
     const QByteArray data = d->ioDevice->read(size);
-    if (data.isEmpty() && !d->ioDevice->atEnd()) {
-        error(Phonon::NormalError, d->ioDevice->errorString());
-    }
+#ifdef __GNUC__
+#warning TODO 4.5 - make sure we do not break anything without this, it is preventing IODs from working when they did not yet emit readyRead()
+#endif
+//    if (data.isEmpty() && !d->ioDevice->atEnd()) {
+//        error(Phonon::NormalError, d->ioDevice->errorString());
+//    }
     writeData(data);
     if (d->ioDevice->atEnd()) {
         endOfData();
