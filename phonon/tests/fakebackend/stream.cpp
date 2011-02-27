c9e05eeab1f8e0aec36869bca1d9b486c351e479
Harald Sitter <sitter@kde.org>
2011-02-27 04:05:07 +0100

    warning--

--- phonon/tests/fakebackend/stream.cpp
+++ phonon/tests/fakebackend/stream.cpp
@@ -6,7 +6,7 @@
     License as published by the Free Software Foundation; either
     version 2.1 of the License, or (at your option) version 3, or any
     later version accepted by the membership of KDE e.V. (or its
-    successor approved by the membership of KDE e.V.), Nokia Corporation 
+    successor approved by the membership of KDE e.V.), Nokia Corporation
     (or its successors, if any) and the KDE Free Qt Foundation, which shall
     act as a proxy defined in Section 6 of version 3 of the license.
 
@@ -15,7 +15,7 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public 
+    You should have received a copy of the GNU Lesser General Public
     License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 */
@@ -99,5 +99,5 @@ void Stream::consumeStream(int msec)
 } // namespace Fake
 } // namespace Phonon
 
-#include "moc_stream.cpp"
+//#include "moc_stream.cpp"
 // vim: sw=4 sts=4 et tw=100
