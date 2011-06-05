648aa62ebafd0e97ad7db3aed75407448cbf4d03
Harald Sitter <sitter@kde.org>
2011-06-05 15:01:09 +0200

    change sifp ctor to expect the q ptr as argument

--- phonon/streaminterface_p.h
+++ phonon/streaminterface_p.h
@@ -6,7 +6,7 @@
     License as published by the Free Software Foundation; either
     version 2.1 of the License, or (at your option) version 3, or any
     later version accepted by the membership of KDE e.V. (or its
-    successor approved by the membership of KDE e.V.), Nokia Corporation 
+    successor approved by the membership of KDE e.V.), Nokia Corporation
     (or its successors, if any) and the KDE Free Qt Foundation, which shall
     act as a proxy defined in Section 6 of version 3 of the license.
 
@@ -15,7 +15,7 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public 
+    You should have received a copy of the GNU Lesser General Public
     License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 */
@@ -39,8 +39,13 @@ class StreamInterfacePrivate
         void disconnectMediaStream();
 
     protected:
-        inline StreamInterfacePrivate()
-            : connected(false)
+        inline StreamInterfacePrivate(StreamInterface *q_) :
+            q(q_),
+            connected(false)
+        {
+        }
+
+        inline virtual ~StreamInterfacePrivate()
         {
         }
 
