KDE/phonon

commit 84e093738a193c5492c7453f27869fef8137d4ab

make capture enum naming consistent

phonon/phononnamespace.cpp
@@ -15,7 +15,7 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public 
+    You should have received a copy of the GNU Lesser General Public
     License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 */
@@ -76,11 +76,11 @@ namespace Phonon
         case Phonon::NoCategory:
             break;
         case Phonon::CommunicationCategory:
-            return QCoreApplication::translate("Phonon::", "Communication");
-        case Phonon::RecordingCategory:
-            return QCoreApplication::translate("Phonon::", "Recording");
-        case Phonon::ControlCategory:
-            return QCoreApplication::translate("Phonon::", "Control");
+            return QCoreApplication::translate("Phonon::", "CaptureCommunication");
+        case Phonon::RecordingCaptureCategory:
+            return QCoreApplication::translate("Phonon::", "CaptureRecording");
+        case Phonon::ControlCaptureCategory:
+            return QCoreApplication::translate("Phonon::", "CaptureControl");
         }
         return QString();
     }

phonon/phononnamespace.h.in
@@ -6,7 +6,7 @@
     License as published by the Free Software Foundation; either
     version 2.1 of the License, or (at your option) version 3, or any
     later version accepted by the membership of KDE e.V. (or its
-    successor approved by the membership of KDE e.V.), Nokia Corporation 
+    successor approved by the membership of KDE e.V.), Nokia Corporation
     (or its successors, if any) and the KDE Free Qt Foundation, which shall
     act as a proxy defined in Section 6 of version 3 of the license.
 
@@ -15,7 +15,7 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public 
+    You should have received a copy of the GNU Lesser General Public
     License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 */
@@ -242,12 +242,12 @@ namespace Phonon
         /**
          * Recording applications, that actually save the data somewhere
          */
-        RecordingCategory,
+        RecordingCaptureCategory,
 
         /**
          * Voice control. Maybe used for accesibility purpouses too
          */
-        ControlCategory = AccessibilityCategory
+        ControlCaptureCategory = AccessibilityCategory
     };
 
     /**

phonon/pulsesupport.cpp
@@ -156,8 +156,8 @@ static QMap<QString, PulseStream*> s_outputStreams;
 static const Phonon::CaptureCategory s_audioCapCategories[] = {
     Phonon::NoCaptureCategory,
     Phonon::CommunicationCaptureCategory,
-    Phonon::RecordingCategory,
-    Phonon::ControlCategory
+    Phonon::RecordingCaptureCategory,
+    Phonon::ControlCaptureCategory
 };
 
 static const int s_audioCapCategoriesCount = sizeof(s_audioCapCategories) / sizeof(Phonon::CaptureCategory);
@@ -728,11 +728,14 @@ PulseSupport::PulseSupport()
     //s_roleCategoryMap[QLatin1String("production")]; // No Mapping
     s_roleCategoryMap[QLatin1String("a11y")] = Phonon::AccessibilityCategory;
 
+#if __GNUC__
+#warning TODO 4.5 - this looks fishy
+#endif
     // Same thing for capture
     s_roleCaptureCategoryMap[QLatin1String("none")] = Phonon::NoCaptureCategory;
     s_roleCaptureCategoryMap[QLatin1String("phone")] = Phonon::CommunicationCaptureCategory;
-    s_roleCaptureCategoryMap[QLatin1String("production")] = Phonon::RecordingCategory;
-    s_roleCaptureCategoryMap[QLatin1String("a11y")] = Phonon::ControlCategory;
+    s_roleCaptureCategoryMap[QLatin1String("production")] = Phonon::RecordingCaptureCategory;
+    s_roleCaptureCategoryMap[QLatin1String("a11y")] = Phonon::ControlCaptureCategory;
 
     // To allow for easy debugging, give an easy way to disable this pulseaudio check
     QByteArray pulseenv = qgetenv("PHONON_PULSEAUDIO_DISABLE");

commit 9f22dbd23c9cb9efa16331e9d1e064ba83cefdad

Fix the nice names for the capture enume broken by Harald in 84e0937
Likely a sed or global find/replace thinko

phonon/phononnamespace.cpp
@@ -76,11 +76,11 @@ namespace Phonon
         case Phonon::NoCategory:
             break;
         case Phonon::CommunicationCategory:
-            return QCoreApplication::translate("Phonon::", "CaptureCommunication");
+            return QCoreApplication::translate("Phonon::", "Communication");
         case Phonon::RecordingCaptureCategory:
-            return QCoreApplication::translate("Phonon::", "CaptureRecording");
+            return QCoreApplication::translate("Phonon::", "Recording");
         case Phonon::ControlCaptureCategory:
-            return QCoreApplication::translate("Phonon::", "CaptureControl");
+            return QCoreApplication::translate("Phonon::", "Control");
         }
         return QString();
     }