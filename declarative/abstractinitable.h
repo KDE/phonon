ae306a9e764a202160ee908eb92925cb254d2d20
Harald Sitter <sitter@kde.org>
2011-08-22 14:28:14 +0200

    documentation++

--- declarative/abstractinitable.h
+++ declarative/abstractinitable.h
@@ -19,8 +19,8 @@
     License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef ABSTRACTINITABLE_H
-#define ABSTRACTINITABLE_H
+#ifndef PHONON_DECLARATIVE_ABSTRACTINITABLE_H
+#define PHONON_DECLARATIVE_ABSTRACTINITABLE_H
 
 #include <QtCore/QObject>
 
@@ -30,18 +30,97 @@ class MediaObject;
 
 namespace Declarative {
 
+/**
+ * Helps with lazy init protection.
+ * It checks whether a specified member variable evaluates to false (null pointer,
+ * false bool...) and if it does returns a default initialized instance of a
+ * specified return type.
+ *
+ * \param member the member variable to check
+ * \param ret the return type to return on failure
+ */
 #define P_D_PROTECT(member, ret) if (!member) return ret()
+
+/**
+ * Helps with lazy init protection.
+ * It checks whether a specified member variable evaluates to false (null pointer,
+ * false bool...) and if it does returns specified return value.
+ *
+ * \param member the member variable to check
+ * \param ret the return value to return on failure
+ */
 #define P_D_PROTECT_VALUE(member, ret) if (!member) return ret
 
+/**
+ * This class allows the initialization of QObjects in an object tree.
+ * All Phonon declarative items are derived from an AbstractInitAble and something
+ * based on QObject (one way or another). They implement the init function which
+ * usually receives a MediaObject instance as argument. Mostly a MediaElement will
+ * kick off the init chain by calling initChildren() whcih consequently calls
+ * init() on all children that are AbstractInitAble.
+ *
+ * This enables implicit Phonon Path creation by implicit nesting:
+ * \code
+ * Media {
+ *     Audio {}
+ *     Video { Subtitle {} }
+ * }
+ * \endcode
+ *
+ * Which roughly translates to a call chain like this:
+ * \verbatim
+ * MediaElement::componentComplete()
+ *  MediaElement::initChildren()
+ *   AudioElement::init()
+ *   VideoElement::init()
+ *    VideoElement::initChildren()
+ *     SubtitleElement::init()
+ * \endverbatim
+ *
+ * Within each init() the element creates a patch between the MO and its own
+ * Phonon MediaNode. In a decorator element such as the subtitle it will get its
+ * parentItem and try to obtain the appropriate Phonon MediaNode from it (if
+ * necessary at all).
+ *
+ * The explicit check of the parent is necessary to ensure that the nesting
+ * represents a valid path and also since most decorators will not
+ * actually decorate its parent but in fact the MediaObject.
+ * This is particularly true since most Video decorators are actually bundled in
+ * the Phonon::MediaController class which attaches to a MediaObject.
+ *
+ * \author Harald Sitter <sitter@kde.org>
+ */
 class AbstractInitAble
 {
 public:
+    /**
+     * Called by the parent item upon init. This can be either once the
+     * component is complete or at any given point later in the life time of the
+     * component.
+     *
+     * There are two options to handle init. One is to create the underlying
+     * Phonon MediaNode right after creation of your instance or at a declarative
+     * parser status update. The other is to only do it once init() is called.
+     *
+     * While the latter option permits startup improvements due to lazy init()
+     * up until the point when the user actually tries to use the Phonon graph
+     * it also requires you to protect your class from accesses to unitialized
+     * members before init() was called.
+     *
+     * To help with the protection there are the macros P_D_PROTECT and
+     * P_D_PROTECT_VALUE that help with protecting a member by either returning
+     * a default constructed object or a given type.
+     */
     virtual void init(MediaObject *mediaObject = 0) = 0;
 
 protected:
     AbstractInitAble() : m_mediaObject(0) {}
     virtual ~AbstractInitAble() {}
 
+    /**
+     * Traverse child objects and try to cast them to AbstractInitAble. On success
+     * call init with the own m_mediaObject.
+     */
     inline void initChildren(QObject *that) const
     {
         foreach (QObject *qobject, that->children()) {
@@ -57,4 +136,4 @@ protected:
 } // namespace Declarative
 } // namespace Phonon
 
-#endif // ABSTRACTINITABLE_H
+#endif // PHONON_DECLARATIVE_ABSTRACTINITABLE_H
