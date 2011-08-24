3f651883fdb34053d2b1936995d773d6e7bdc6bd
Martin T. H. Sandsmark <sandsmark@samfundet.no>
2011-08-24 19:55:09 +0200

    Re-add a very basic test structure.

---
+++ tests/unit/backend.cpp
@@ -0,0 +1,16 @@
+#include <QObject>
+#include <QTest>
+
+class tst_Backend : public QObject
+{
+private slots:
+    void creation();
+};
+
+void tst_Backend::creation()
+{
+}
+
+QTEST_MAIN(tst_Backend)
+
+
