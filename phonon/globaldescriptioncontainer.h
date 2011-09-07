be044bcf0e32c38da70ae79c36a1eb6c2253414d
Andrius da Costa Ribas <andriusmao@gmail.com>
2011-09-07 18:26:44 -0300

    Amending last commit, using Q_FUNC_INFO, as suggested by Harald.

--- phonon/globaldescriptioncontainer.h
+++ phonon/globaldescriptioncontainer.h
@@ -252,15 +252,9 @@ public:
         Q_ASSERT(obj);
         Q_ASSERT(m_localIds.find(obj) != m_localIds.end());
         if (m_localIds[obj].find(key) == m_localIds[obj].end())
-#if defined(__PRETTY_FUNCTION__)
-            qWarning() << "WARNING:" << __PRETTY_FUNCTION__
+            qWarning() << "WARNING:" << Q_FUNC_INFO
                        << ": supplied global ID is unknown for the object ("
                        << obj << ")";
-#elif defined(__FUNCSIG__)
-            qWarning() << "WARNING:" << __FUNCSIG__
-                       << ": supplied global ID is unknown for the object ("
-                       << obj << ")";
-#endif
         return m_localIds[obj].value(key, 0);
     }
 
