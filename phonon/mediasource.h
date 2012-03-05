KDE/phonon

commit dbe4307b1313b35c22050cc165bfa22aec92a4af

Introduce a new type of MediaSource, for AVCapture
This type of media source describes an audio device and a video device
together. It's purpouse is to enable the backends to implement audio
video capture, for example capturing video from a webcam and audio from
a microphone, at the same time.

MediaSourcePrivate now has separate device access lists.

Deprecated an old unused protected constructor that stood in the way.

This commit tries to preserve source / abi compatibility.

phonon/mediasource.h
@@ -97,9 +97,18 @@ class PHONON_EXPORT MediaSource
              */
             Stream,
             /**
-            * The MediaSource object describes a capture device
+            * The MediaSource object describes a single capture device.
+            * This could be either audio or video.
             */
             CaptureDevice,
+            /**
+             * The MediaSource object describes one device for video capture and one for audio
+             * capture. Facilitates capturing both audio and video at the same time, from
+             * different devices.
+             * It's essentially like two CaptureDevice media sources (one of video type, one
+             * of audio type) merged together.
+             */
+            AudioVideoCapture,
             /**
              * An empty MediaSource.
              *
@@ -153,6 +162,7 @@ class PHONON_EXPORT MediaSource
 #ifndef PHONON_NO_AUDIOCAPTURE
         /**
         * Creates a MediaSource object for audio capture devices.
+        * If the device is valid, this creates a 'CaptureDevice' type MediaSource.
         */
         MediaSource(const AudioCaptureDevice& device);
 #endif
@@ -160,6 +170,7 @@ class PHONON_EXPORT MediaSource
 #ifndef PHONON_NO_VIDEOCAPTURE
         /**
         * Creates a MediaSource object for video capture devices.
+        * If the device is valid, this creates a 'CaptureDevice' type MediaSource
         */
         MediaSource(const VideoCaptureDevice& device);
 #endif
@@ -167,8 +178,21 @@ class PHONON_EXPORT MediaSource
 #if !defined(PHONON_NO_VIDEOCAPTURE) && !defined(PHONON_NO_AUDIOCAPTURE)
         /**
          * Sets the source to the preferred audio capture device for the specified category
+         * If a valid device is found, this creates a 'CaptureDevice' type MediaSource
          */
         MediaSource(Capture::DeviceType deviceType, CaptureCategory category = NoCaptureCategory);
+
+        /**
+         * Creates a MediaSource object that tries to describe a video capture device and
+         * an audio capture device, together. The devices are appropriate for the specified
+         * category.
+         *
+         * If valid devices are found for both audio and video, then the resulting MediaSource
+         * is of type 'AudioVideoCapture'. If only an audio or a video valid device is found,
+         * the resulting type is 'CaptureDevice'. If no valid devices are found, the resulting
+         * type is 'Invalid'.
+         */
+        MediaSource(CaptureCategory category);
 #endif
 
 #ifndef QT_NO_PHONON_ABSTRACTMEDIASTREAM
@@ -282,8 +306,23 @@ class PHONON_EXPORT MediaSource
 
         /**
          * Returns the access list for the device of this media source. Valid for capture devices.
+         * \warning use only with MediaSource with type() == CaptureDevice
          */
         const DeviceAccessList& deviceAccessList() const;
+
+        /**
+         * Returns the access list for the video device used for capture.
+         * Valid for type() == CaptureDevice or type() == AudioVideoCapture.
+         * If used with CaptureDevice, the kind of device should be Video, for a valid result.
+         */
+        const DeviceAccessList& videoDeviceAccessList() const;
+
+        /**
+         * Returns the access list for the audio device used for capture.
+         * Valid for type() == CaptureDevice or type() == AudioVideoCapture.
+         * If used with CaptureDevice, the kind of device should be Audio, for a valid result.
+         */
+        const DeviceAccessList& audioDeviceAccessList() const;
 
         /**
          * Returns the device name of the MediaSource if type() == Disc; otherwise returns
@@ -322,12 +361,7 @@ class PHONON_EXPORT MediaSource
         QExplicitlySharedDataPointer<MediaSourcePrivate> d;
         MediaSource(MediaSourcePrivate &);
 
-        /**
-         * Creates a MediaSource object for a capture device, directly from the device access info.
-         *
-         * \param access How can the device be accessed (driver name - alsa, oss, etc.) and device name
-         */
-        MediaSource(const DeviceAccess &access);
+        PHONON_DEPRECATED MediaSource(const DeviceAccess &access);
 };
 
 } // namespace Phonon

phonon/mediasource.cpp
@@ -95,15 +95,10 @@ MediaSource::MediaSource(DiscType dt, const QString &deviceName)
     d->deviceName = deviceName;
 }
 
-// NOTE: this is a protected constructor
-MediaSource::MediaSource(const DeviceAccess &access)
-    : d(new MediaSourcePrivate(CaptureDevice))
+// NOTE: this is deprecated
+MediaSource::MediaSource(const DeviceAccess &)
+    : d(new MediaSourcePrivate(Invalid))
 {
-    if (access.first.isEmpty() || access.second.isEmpty()) {
-        d->type = Invalid;
-        return;
-    }
-    d->deviceAccessList.append(access);
 }
 
 #ifndef PHONON_NO_AUDIOCAPTURE
@@ -123,6 +118,12 @@ MediaSource::MediaSource(const VideoCaptureDevice& device)
 #endif //PHONON_NO_VIDEOCAPTURE
 
 #if !defined(PHONON_NO_VIDEOCAPTURE) && !defined(PHONON_NO_AUDIOCAPTURE)
+MediaSource::MediaSource(CaptureCategory category)
+    : d(new MediaSourcePrivate(AudioVideoCapture))
+{
+    d->setCaptureDevices(category);
+}
+
 MediaSource::MediaSource(Capture::DeviceType deviceType, CaptureCategory category)
     : d(new MediaSourcePrivate(CaptureDevice))
 {
@@ -245,7 +246,23 @@ DiscType MediaSource::discType() const
 
 const DeviceAccessList& MediaSource::deviceAccessList() const
 {
-    return d->deviceAccessList;
+    if (d->audioCaptureDevice.isValid())
+        return d->audioDeviceAccessList;
+
+    if (d->videoCaptureDevice.isValid())
+        return d->videoDeviceAccessList;
+
+    return d->audioDeviceAccessList;    // It should be invalid
+}
+
+const DeviceAccessList& MediaSource::audioDeviceAccessList() const
+{
+    return d->audioDeviceAccessList;
+}
+
+const DeviceAccessList& MediaSource::videoDeviceAccessList() const
+{
+    return d->videoDeviceAccessList;
 }
 
 QString MediaSource::deviceName() const
@@ -296,6 +313,13 @@ void MediaSourcePrivate::setCaptureDevice(Capture::DeviceType deviceType, Captur
     }
 }
 
+void MediaSourcePrivate::setCaptureDevices(CaptureCategory category)
+{
+    setCaptureDevices(
+        AudioCaptureDevice::fromIndex(GlobalConfig().audioCaptureDeviceFor(category)),
+        VideoCaptureDevice::fromIndex(GlobalConfig().videoCaptureDeviceFor(category)));
+}
+
 void MediaSourcePrivate::setCaptureDevices(const AudioCaptureDevice &audioDevice, const VideoCaptureDevice &videoDevice)
 {
     audioCaptureDevice = audioDevice;
@@ -303,15 +327,21 @@ void MediaSourcePrivate::setCaptureDevices(const AudioCaptureDevice &audioDevice
 
     if (audioDevice.propertyNames().contains("deviceAccessList") &&
             !audioDevice.property("deviceAccessList").value<DeviceAccessList>().isEmpty()) {
-        deviceAccessList = audioDevice.property("deviceAccessList").value<DeviceAccessList>();
+        audioDeviceAccessList = audioDevice.property("deviceAccessList").value<DeviceAccessList>();
     }
 
     if (videoDevice.propertyNames().contains("deviceAccessList") &&
             !videoDevice.property("deviceAccessList").value<DeviceAccessList>().isEmpty()) {
-        deviceAccessList = videoDevice.property("deviceAccessList").value<DeviceAccessList>();
+        videoDeviceAccessList = videoDevice.property("deviceAccessList").value<DeviceAccessList>();
     }
 
-    type = deviceAccessList.isEmpty() ? MediaSource::Invalid : MediaSource::CaptureDevice;
+    bool validAudio = !audioDeviceAccessList.isEmpty();
+    bool validVideo = !videoDeviceAccessList.isEmpty();
+    type = MediaSource::Invalid;
+    if (validAudio && validVideo)
+        type = MediaSource::AudioVideoCapture;
+    else if (validAudio || validVideo)
+        type = MediaSource::CaptureDevice;
 }
 #endif // !PHONON_NO_VIDEOCAPTURE && !PHONON_NO_AUDIOCAPTURE
 

phonon/mediasource_p.h
@@ -63,7 +63,8 @@ class PHONON_EXPORT MediaSourcePrivate : public QSharedData
         Mrl url;
         Phonon::DiscType discType;
         QString deviceName;                         // Used for discs
-        Phonon::DeviceAccessList deviceAccessList;  // Used for capture devices
+        Phonon::DeviceAccessList audioDeviceAccessList;
+        Phonon::DeviceAccessList videoDeviceAccessList;
 
 #ifndef QT_NO_PHONON_ABSTRACTMEDIASTREAM
         // The AbstractMediaStream(2) may be deleted at any time by the application. If that happens
@@ -77,6 +78,7 @@ class PHONON_EXPORT MediaSourcePrivate : public QSharedData
 
 #if !defined(PHONON_NO_VIDEOCAPTURE) && !defined(PHONON_NO_AUDIOCAPTURE)
         void setCaptureDevice(Capture::DeviceType deviceType, CaptureCategory category);
+        void setCaptureDevices(CaptureCategory category);
         void setCaptureDevices(const AudioCaptureDevice &audioDevice, const VideoCaptureDevice &videoDevice);
 #endif // !PHONON_NO_VIDEOCAPTURE && !PHONON_NO_AUDIOCAPTURE
 

commit f6b75e4affddc292b08323954310751c368e6944

Fix broken MediaSource::Type enum, my bad
abi compat was broken by commit dbe4307b1313b35c22050cc165bfa22aec92a4af

phonon/mediasource.h
@@ -102,21 +102,21 @@ class PHONON_EXPORT MediaSource
             */
             CaptureDevice,
             /**
-             * The MediaSource object describes one device for video capture and one for audio
-             * capture. Facilitates capturing both audio and video at the same time, from
-             * different devices.
-             * It's essentially like two CaptureDevice media sources (one of video type, one
-             * of audio type) merged together.
-             */
-            AudioVideoCapture,
-            /**
              * An empty MediaSource.
              *
              * It can be used to unload the current media from a MediaObject.
              *
              * \see MediaSource()
              */
-            Empty
+            Empty,
+            /**
+             * The MediaSource object describes one device for video capture and one for audio
+             * capture. Facilitates capturing both audio and video at the same time, from
+             * different devices.
+             * It's essentially like two CaptureDevice media sources (one of video type, one
+             * of audio type) merged together.
+             */
+            AudioVideoCapture
 /*          post 4.0:
             / **
              * Links multiple MediaSource objects together.