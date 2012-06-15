961e319fbe2fe85add93b5a0153e04404a2850e8
Trever Fischer <tdfischer@fedoraproject.org>
2012-06-15 08:29:20 -0400

    Remove the apparently never used PHONON_NO_DEBUG, as enabling it causes more issues than it solves.

--- phonon/phononnamespace.cpp
+++ phonon/phononnamespace.cpp
@@ -84,119 +84,117 @@ namespace Phonon
         }
         return QString();
     }
-}
 
-#ifndef PHONON_NO_DEBUG
-QDebug operator <<(QDebug dbg, const Phonon::DiscType &type)
-{
-    switch (type) {
-    case Phonon::NoDisc:
-        dbg.space() << "Phonon::NoDisc";
-        break;
-    case Phonon::Cd:
-        dbg.space() << "Phonon::Cd";
-        break;
-    case Phonon::Dvd:
-        dbg.space() << "Phonon::Dvd";
-        break;
-    case Phonon::Vcd:
-        dbg.space() << "Phonon::Vcd";
-        break;
+    QDebug operator <<(QDebug dbg, const Phonon::DiscType &type)
+    {
+        switch (type) {
+        case Phonon::NoDisc:
+            dbg.space() << "Phonon::NoDisc";
+            break;
+        case Phonon::Cd:
+            dbg.space() << "Phonon::Cd";
+            break;
+        case Phonon::Dvd:
+            dbg.space() << "Phonon::Dvd";
+            break;
+        case Phonon::Vcd:
+            dbg.space() << "Phonon::Vcd";
+            break;
+        }
+        return dbg.maybeSpace();
     }
-    return dbg.maybeSpace();
-}
 
-QDebug operator <<(QDebug dbg, const Phonon::MetaData &metaData)
-{
-    switch (metaData) {
-    case Phonon::ArtistMetaData:
-        dbg.space() << "Phonon::ArtistMetaData";
-        break;
-    case Phonon::AlbumMetaData:
-        dbg.space() << "Phonon::AlbumMetaData";
-        break;
-    case Phonon::TitleMetaData:
-        dbg.space() << "Phonon::TitleMetaData";
-        break;
-    case Phonon::DateMetaData:
-        dbg.space() << "Phonon::DateMetaData";
-        break;
-    case Phonon::GenreMetaData:
-        dbg.space() << "Phonon::GenreMetaData";
-        break;
-    case Phonon::TracknumberMetaData:
-        dbg.space() << "Phonon::TracknumberMetaData";
-        break;
-    case Phonon::DescriptionMetaData:
-        dbg.space() << "Phonon::DescriptionMetaData";
-        break;
-    case Phonon::MusicBrainzDiscIdMetaData:
-        dbg.space() << "Phonon::MusicBrainzDiscIdMetaData";
-        break;
+    QDebug operator <<(QDebug dbg, const Phonon::MetaData &metaData)
+    {
+        switch (metaData) {
+        case Phonon::ArtistMetaData:
+            dbg.space() << "Phonon::ArtistMetaData";
+            break;
+        case Phonon::AlbumMetaData:
+            dbg.space() << "Phonon::AlbumMetaData";
+            break;
+        case Phonon::TitleMetaData:
+            dbg.space() << "Phonon::TitleMetaData";
+            break;
+        case Phonon::DateMetaData:
+            dbg.space() << "Phonon::DateMetaData";
+            break;
+        case Phonon::GenreMetaData:
+            dbg.space() << "Phonon::GenreMetaData";
+            break;
+        case Phonon::TracknumberMetaData:
+            dbg.space() << "Phonon::TracknumberMetaData";
+            break;
+        case Phonon::DescriptionMetaData:
+            dbg.space() << "Phonon::DescriptionMetaData";
+            break;
+        case Phonon::MusicBrainzDiscIdMetaData:
+            dbg.space() << "Phonon::MusicBrainzDiscIdMetaData";
+            break;
+        }
+        return dbg.maybeSpace();
     }
-    return dbg.maybeSpace();
-}
 
-QDebug operator <<(QDebug dbg, const Phonon::State &state)
-{
-    switch (state) {
-    case Phonon::LoadingState:
-        dbg.space() << "Phonon::LoadingState";
-        break;
-    case Phonon::StoppedState:
-        dbg.space() << "Phonon::StoppedState";
-        break;
-    case Phonon::PlayingState:
-        dbg.space() << "Phonon::PlayingState";
-        break;
-    case Phonon::BufferingState:
-        dbg.space() << "Phonon::BufferingState";
-        break;
-    case Phonon::PausedState:
-        dbg.space() << "Phonon::PausedState";
-        break;
-    case Phonon::ErrorState:
-        dbg.space() << "Phonon::ErrorState";
-        break;
+    QDebug operator <<(QDebug dbg, const Phonon::State &state)
+    {
+        switch (state) {
+        case Phonon::LoadingState:
+            dbg.space() << "Phonon::LoadingState";
+            break;
+        case Phonon::StoppedState:
+            dbg.space() << "Phonon::StoppedState";
+            break;
+        case Phonon::PlayingState:
+            dbg.space() << "Phonon::PlayingState";
+            break;
+        case Phonon::BufferingState:
+            dbg.space() << "Phonon::BufferingState";
+            break;
+        case Phonon::PausedState:
+            dbg.space() << "Phonon::PausedState";
+            break;
+        case Phonon::ErrorState:
+            dbg.space() << "Phonon::ErrorState";
+            break;
+        }
+        return dbg.maybeSpace();
     }
-    return dbg.maybeSpace();
-}
 
-QDebug operator <<(QDebug dbg, const Phonon::Category &category)
-{
-    dbg.space() << categoryToString(category);
-    return dbg.maybeSpace();
-}
-
-QDebug operator <<(QDebug dbg, const Phonon::Capture::DeviceType &type)
-{
-    switch (type) {
-    case Phonon::Capture::AudioType:
-        dbg.space() << "Phonon::Capture::AudioType";
-        break;
-    case Phonon::Capture::VideoType:
-        dbg.space() << "Phonon::Capture::VideoType";
-        break;
+    QDebug operator <<(QDebug dbg, const Phonon::Category &category)
+    {
+        dbg.space() << categoryToString(category);
+        return dbg.maybeSpace();
     }
-    return dbg.maybeSpace();
-}
 
-QDebug operator <<(QDebug dbg, const Phonon::ErrorType &errorType)
-{
-    switch (errorType) {
-    case Phonon::NoError:
-        dbg.space() << "Phonon::NoError";
-        break;
-    case Phonon::NormalError:
-        dbg.space() << "Phonon::NormalError";
-        break;
-    case Phonon::FatalError:
-        dbg.space() << "Phonon::FatalError";
-        break;
+    QDebug operator <<(QDebug dbg, const Phonon::Capture::DeviceType &type)
+    {
+        switch (type) {
+        case Phonon::Capture::AudioType:
+            dbg.space() << "Phonon::Capture::AudioType";
+            break;
+        case Phonon::Capture::VideoType:
+            dbg.space() << "Phonon::Capture::VideoType";
+            break;
+        }
+        return dbg.maybeSpace();
     }
-    return dbg.maybeSpace();
+
+    QDebug operator <<(QDebug dbg, const Phonon::ErrorType &errorType)
+    {
+        switch (errorType) {
+        case Phonon::NoError:
+            dbg.space() << "Phonon::NoError";
+            break;
+        case Phonon::NormalError:
+            dbg.space() << "Phonon::NormalError";
+            break;
+        case Phonon::FatalError:
+            dbg.space() << "Phonon::FatalError";
+            break;
+        }
+        return dbg.maybeSpace();
+    }
 }
-#endif // PHONON_NO_DEBUG
 
 static int registerPhononMetaTypes()
 {
