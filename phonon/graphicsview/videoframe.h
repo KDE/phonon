2e33ab4b7161e89ea55616d10a5d358009ffe7ac
Harald Sitter <sitter@kde.org>
2012-07-16 09:30:33 +0200

    add pitch[], visiblePitch[], lines[], visibleLines[] to enable
    pitch dependent drawing/clipping

--- phonon/graphicsview/videoframe.h
+++ phonon/graphicsview/videoframe.h
@@ -49,7 +49,7 @@ struct VideoFrame {
         Format_RGB32,   /** < Packed RGB32 */
         Format_YV12,    /** < 3 planes 8 bit Y plane followed by 8 bit 2x2
                               subsampled V and U planes. */
-        Format_I420     /** < Like YV12, but U comes before V */
+        Format_I420     /** < Like YV12, but U and V are swapped */
     };
 
     /// The width.
@@ -66,6 +66,36 @@ struct VideoFrame {
 
     /// The picture planes.
     QByteArray plane[4];
+
+    /**
+     * Pitch/stride of a plane.
+     * This pitch is aligned at the backend's desire, so you must not assume any
+     * particular alignment.
+     * @note The pitch is always bigger than the visible pitch, for painting you
+     *       want to clip the plane accordingly (e.g. treat the visible pitch as
+     *       a subpicture).
+     * @note for conversion into a QImage you will have to pass the appropriate
+     *       pitch as bytesPerLine or you'll get unexpected results.
+     */
+    int pitch[4];
+
+    /**
+     * The visible pitch is the actually visible pitch of a plane.
+     * It depends only on the format (e.g. for YV12 0 = width, 1 = 2 = width/2).
+     */
+    int visiblePitch[4];
+
+    /**
+     * Lines/scanlines in the plane (aligned).
+     * @see pitch
+     */
+     int lines[4];
+
+    /**
+     * Visible lines/scanlines in the plane.
+     * @see visiblePitch
+     */
+    int visibleLines[4];
 };
 
 } // namespace Phonon
