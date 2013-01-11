8a5f570f2f3219849a8f3566310f7fdc326541f4
Harald Sitter <sitter@kde.org>
2013-01-11 12:54:42 +0100

    update designer plugin macro for qt5

--- cmake/PhononMacros.cmake
+++ cmake/PhononMacros.cmake
@@ -29,7 +29,7 @@ endmacro(phonon_add_declarative_plugin _target)
 
 macro(phonon_add_designer_plugin _target _qrc_file)
     set(_srcs ${ARGN})
-    qt4_add_resources(_srcs ${_qrc_file})
+    qt5_add_resources(_srcs ${_qrc_file})
     automoc4_add_library(${_target} MODULE ${_global_add_executable_param} ${_srcs})
 endmacro(phonon_add_designer_plugin)
 
