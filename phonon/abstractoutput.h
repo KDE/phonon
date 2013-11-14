186e67fa9273e0eb4695d239bdfa73526b0dba35
Harald Sitter <sitter@kde.org>
2013-11-14 12:45:56 +0100

    Revert "fixed friending yesterday"
    This reverts commit 576d16a599f2cb2b4b392b8063ae8fecf8030f59.

--- phonon/abstractoutput.h
+++ phonon/abstractoutput.h
@@ -30,6 +30,8 @@ namespace Phonon {
 class AbstractOutputPrivate;
 class PHONON_EXPORT AbstractOutput : public Frontend
 {
+#warning perhaps find a way to avoid friending player
+    friend class Player;
 public:
     virtual ~AbstractOutput();
 
