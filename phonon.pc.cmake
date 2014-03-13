71f08abdad3ac724d4a6354720da255c134cf1fa
Harald Sitter <sitter@kde.org>
2014-03-13 10:42:24 +0100

    port pkgconfig generation and cleanup include dirs

--- phonon.pc.cmake
+++ phonon.pc.cmake
@@ -1,12 +1,12 @@
 prefix=@CMAKE_INSTALL_PREFIX@
-exec_prefix=@EXEC_INSTALL_PREFIX@
-libdir=@ABS_LIB_INSTALL_DIR@
-includedir=@ABS_INCLUDE_INSTALL_DIR@
-buildsystemdir=@ABS_BUILDSYSTEM_INSTALL_DIR@
+exec_prefix=${prefix}
+libdir=@CMAKE_INSTALL_FULL_LIBDIR@
+includedir=@CMAKE_INSTALL_FULL_INCLUDEDIR@/@PHONON_LIB_SONAME@
 
 Name: @PHONON_LIB_SONAME_CAMEL@
 Description: Phonon library needed to build applications
-Version: @PHONON_LIB_MAJOR_VERSION@.@PHONON_LIB_MINOR_VERSION@.@PHONON_LIB_PATCH_VERSION@
-Requires: QtCore QtGui QtDBus
+Version: @PHONON_VERSION_MAJOR@.@PHONON_VERSION_MINOR@.@PHONON_VERSION_PATCH@
+# FIXME: it might be nice to generate these from the target itself...
+Requires: Qt5Core, Qt5Gui
 Libs: -L${libdir} -l@PHONON_LIB_SONAME@
 Cflags: -I${includedir}
