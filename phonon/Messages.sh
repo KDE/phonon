6ad3d6d2eb55b74c246b59d717c25a48c00fd796
Jonathan Esk-Riddell <jr@jriddell.org>
2023-05-17 15:43:01 +0100

    allow for both qt5 and qt6 builds

--- phonon/Messages.sh
+++ phonon/Messages.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 #$EXTRACTRC `find $dirs -maxdepth 1 \( -name \*.rc -o -name \*.ui -o -name \*.ui3 -o -name \*.ui4 -o -name \*.kcfg \) ` >> rc.cpp || exit 11
 $EXTRACT_TR_STRINGS $(find . -maxdepth 1 -name "*.cpp" -o -name "*.h") -o $podir/libphonon6_qt.pot
+$EXTRACT_TR_STRINGS $(find . -maxdepth 1 -name "*.cpp" -o -name "*.h") -o $podir/libphonon_qt.pot
--- settings/Messages.sh
+++ settings/Messages.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 $EXTRACT_TR_STRINGS $(find . -name \*.cpp -o -name \*.h -o -name \*.ui -o -name \*.qml) -o $podir/phononsettings6_qt.pot
+$EXTRACT_TR_STRINGS $(find . -name \*.cpp -o -name \*.h -o -name \*.ui -o -name \*.qml) -o $podir/phononsettings_qt.pot
